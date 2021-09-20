2abbcfffa74016727536e5b6fa555f1b6c96707f
akreese <76131466+akreese@users.noreply.github.com>
2021-09-20 12:17:59 -0700

    longest-equal-sequence

---
+++ longestEqualSequence/README.md
@@ -0,0 +1,3 @@
+When given an input of list of numbers, this method will find the longest sequence of numbers that are equal. 
+For example, in [6, 7, 4, 4, 4, 7, 1, 3, 3, 4, 1, 4, 3, 4, 2, 9, 1, 8, 2, 7], the longest sequence is 4, 4, 4.
+The method would return [4, 4, 4]. If there are ties, return the first one encountered assuming the list is traversed from lowest to highest index.
---
+++ longestEqualSequence/longest.py
@@ -0,0 +1,47 @@
+from collections import defaultdict
+
+def longestEqualSequence(sequence):
+  print(sequence)
+  sequence_dict = defaultdict(int)
+  longest_sequence = 0
+  current_count = 1
+  currest_sequence_list = []
+  longest_sequence_list = []
+  previous_counter = 0
+  for i  in range(len(sequence)):
+    # If the input list is only one element, the longest sequence will be that input list
+    if len(sequence) == 1:
+      longest_sequence_list = sequence
+    if i > 0:
+      # If this is an equal sequence, i.e) [..., 6, 6, ...] previous = 6 and sequence[i] = 6
+      if previous_number == sequence[i]:
+        # Increment the current count of the current sequence by one. 
+        # current_count = current_count + 1
+        current_count += 1
+        # If the element is the last element of the list,
+        # check if the current sequence is the longest equal sequence
+        # if it is, assign the current sequence to the longest_sequence_list
+        if i == len(sequence) - 1:
+          if longest_sequence < current_count:
+            longest_sequence = current_count
+            longest_sequence_list = currest_sequence_list
+      # If this not an equal sequence, i.e)  [..., 1, 6, ...] previous = 1 and sequence[i] = 6
+      else: 
+        # Check if the current longest sequence is the longest equal sequence 
+        # if it is, assing the current sequence to the longest_sequence_list
+        if longest_sequence < current_count:
+          longest_sequence = current_count
+          longest_sequence_list = currest_sequence_list
+        # After checking if the current list is the longest equal sequence,
+        # clear the current equal sequence list
+        currest_sequence_list = []
+        current_count = 1
+    # After each loop, assign the current value to the previous number
+    previous_number = sequence[i]
+    currest_sequence_list.append(sequence[i])
+    
+  return longest_sequence_list
+
+a = [int(x) for x in input("Enter a list of numbers with space in between each number:").split()]
+print("Longest Sequence of the list", a, "is")
+print(longestEqualSequence(a))

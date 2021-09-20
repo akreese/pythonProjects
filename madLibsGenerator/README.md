9f29832eaaf3e68553e6d0b2fc9cfe4fbaddebc1
akreese <76131466+akreese@users.noreply.github.com>
2021-09-20 12:39:42 -0700

    mad-libs-generator

---
+++ madLibsGenerator/README.md
@@ -0,0 +1,1 @@
+Enter in the words as prompted and a story will be generated for you.
---
+++ madLibsGenerator/madlibs.py
@@ -0,0 +1,18 @@
+
+print("Let's play Mad Libs.")
+print("Please enter a word prompted below")
+
+name = input("Male name: ")
+adj1 = input("Adjective 1: ")
+adj2 = input("Adjective 2: ")
+activity1 = input("Activity 1: ")
+activity2 = input("Activity 2: ")
+activity3 = input("Activity 3: ")
+noun1 = input("Noun 1: ")
+noun2 = input("Noun 2: ")
+noun4 = input("Noun 4 (plural): ")
+place1 = input("Place 1: ")
+place2 = input("Place 2: ")
+
+print(name, " knows a ", adj1, noun1, " who likes to visits ", place1, "and buys lots of ", adj2, " gifts for their friends. They always give a gift to", noun2, " who was their best friend in highschool. When " ,name, "isn't out traveling the world, they like to hang out with", noun4, " and do ", activity1, " around town. When ", name, "'s ", noun2, " isn't free to hang out, they ", activity2, "and", activity3, "in", place2, "to keep themselve busy.")
+print("DONE! Did you like your story?")

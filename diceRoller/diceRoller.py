f334562d432952342b81dae9cbb655f78ba7c473
akreese <76131466+akreese@users.noreply.github.com>
2021-09-20 12:43:47 -0700

    dice-roller-update

--- diceRoller/diceRoller.py
+++ diceRoller/diceRoller.py
@@ -3,7 +3,7 @@ again = True
 
 while again:
   print(random.randint(1, 6))
-  new_roll = str(input("Do you want to roll the dice again? "))
+  new_roll = str(input("Do you want to roll the dice again? (y/n) "))
   if new_roll.lower() == "y":
     continue
   else:
